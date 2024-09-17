f7d1cc016d30e1245607e5d48cf10505d67de5cc
ReaGemt <zero2028@inbox.ru>
2024-09-17 10:00:28 +0300

    Add new script and style

--- DJ02/myapp/views.py
+++ DJ02/myapp/views.py
@@ -3,19 +3,19 @@ from django.shortcuts import render
 # Главная страница
 def home(request):
     # Отображает главную страницу
-    return render(request, 'home.html')
+    return render(request, 'myapp/home.html')
 
 # Страница "О нас"
 def about(request):
     # Отображает страницу с информацией о компании и командой
-    return render(request, 'about.html')
+    return render(request, 'myapp/about.html')
 
 # Страница "Наши услуги"
 def services(request):
     # Отображает страницу с перечнем предоставляемых услуг
-    return render(request, 'services.html')
+    return render(request, 'myapp/services.html')
 
 # Страница "Контакты"
 def contact(request):
     # Отображает страницу с контактной информацией
-    return render(request, 'contact.html')
+    return render(request, 'myapp/contact.html')
